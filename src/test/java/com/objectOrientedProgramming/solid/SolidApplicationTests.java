4a5eed0ae0f5f2c439faff88f7596be24149667c
DeboraMendes <debora.mendes@live.com>
2022-12-21 11:03:22 -0300

    Initial commit

---
+++ src/test/java/com/objectOrientedProgramming/solid/SolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectOrientedProgramming.solid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
